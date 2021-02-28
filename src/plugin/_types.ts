e2eaeaad86702abb93e161e83150c64b433ae9fa
Gustavo <kyoro_souza@hotmail.com>
2021-02-28 00:14:13 -0300

    🏷️ add Text props

--- src/plugin/_types.ts
+++ src/plugin/_types.ts
@@ -2,3 +2,9 @@ export interface ControllerProps {
   type: string;
   hex: string | number;
 }
+
+export interface TextProps {
+  type?: "title" | "subtitle";
+  text?: string;
+  position?: any;
+}
