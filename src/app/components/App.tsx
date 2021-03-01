e8097295e89eeb403c8b5101acece0f2d81ae497
Gustavo <kyoro_souza@hotmail.com>
2021-03-01 16:06:59 -0300

    💄 new textfields

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -6,23 +6,37 @@ import { Container, PrimaryButton, Button, Textfield } from "../styles/index";
 declare function require(path: string): any;
 
 const App = ({}) => {
-  // const countTextbox = useRef<HTMLInputElement>(undefined);
+  const titleTextbox = useRef<HTMLInputElement>(undefined);
+  const subtitleTextbox = useRef<HTMLInputElement>(undefined);
   const colorTextbox = useRef<HTMLInputElement>(undefined);
 
-  // const countRef = useCallback((element: HTMLInputElement) => {
-  //   if (element) element.value = "1";
-  //   countTextbox.current = element;
-  // }, []);
+  const textRef = useCallback((element: HTMLInputElement) => {
+    if (element) element.value = "Primary";
+    titleTextbox.current = element;
+  }, []);
+
+  const subtitleRef = useCallback((element: HTMLInputElement) => {
+    if (element) element.value = "This is a description";
+    subtitleTextbox.current = element;
+  }, []);
 
   const colorRef = useCallback((element: HTMLInputElement) => {
     if (element) colorTextbox.current = element;
   }, []);
 
   const onCreate = useCallback(() => {
-    // const count = parseInt(countTextbox.current.value, 10);
+    const title = String(titleTextbox.current.value);
+    const description = String(subtitleTextbox.current.value);
     const color = String(colorTextbox.current.value);
     parent.postMessage(
-      { pluginMessage: { type: "create-card", hex: color } },
+      {
+        pluginMessage: {
+          type: "create-card",
+          hex: color,
+          title: title,
+          description,
+        },
+      },
       "*"
     );
   }, []);
@@ -44,9 +58,12 @@ const App = ({}) => {
   return (
     <Container>
       <h2>Pick a Color</h2>
-      {/* <p>
-        Count: <Textfield ref={countRef} />
-      </p> */}
+      <p>
+        Title: <Textfield ref={textRef} />
+      </p>
+      <p>
+        Descrição: <Textfield ref={subtitleRef} />
+      </p>
       <p>
         Color: <Textfield type="color" ref={colorRef} />
       </p>
